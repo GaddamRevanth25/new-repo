cb79a24cd62c48e70ca6da31a4881243c053a39d
Sk Juveria Tabassum <154406153+juveria2905@users.noreply.github.com>
2024-11-09 16:24:55 +0530

    Update AgeFinder.java

--- AgeFinder.java
+++ AgeFinder.java
@@ -41,6 +41,7 @@ public class AgeFinder {
 	
 
 	public static void main(String[] args) {
+		//revanth the coder
 		Scanner scn = new Scanner(System.in);
 		System.out.println("Enter the Date Of Birth (Format :- yyyy-mm-dd): ");
 		String dateOfBirth = scn.nextLine();
